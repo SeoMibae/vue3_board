560ed13fe52f6f2d65fba329c60f0ac40fd1bd50
SeoMibae <m910205b@gmail.com>
2023-09-21 11:46:00 +0900

    add git

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<h2>Home View</h2>
+		<p>{{ $route.path }}</p>
+		<p>{{ $route.name }}</p>
+		<button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+		<hr class="my-4" />
+		<AppGrid :items="items" v-slot="{ item }">
+			<AppCard>{{ item }}</AppCard>
+		</AppGrid>
+		<hr class="my-4" />
+		<h2>{{ position }}</h2>
+		<h2>x: {{ x }}, y: {{ y }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	created() {
+		// console.log(this.$person.name);
+		// this.$person.say();
+	},
+};
+</script>
+
+<script setup>
+import { reactive, ref, toRefs } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/about');
+};
+
+const items = ref(['사과', '딸기', '포도', '바나나']);
+const position = reactive({
+	x: 100,
+	y: 1000,
+});
+const { x, y } = toRefs(position);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/nested/NestedView.vue
@@ -0,0 +1,31 @@
+// 중첩라우트 push 히스토리 쌓음 , replace 히스토리 쌓지 않음
+<template>
+	<div>
+		<ul class="nav nav-pills">
+			<li class="nav-item">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedOne', replace: true }"
+				>
+					Nested One
+				</RouterLink>
+			</li>
+			<li class="nav-item">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedTwo', replace: true }"
+				>
+					Nested Two
+				</RouterLink>
+			</li>
+		</ul>
+		<hr class="my-4" />
+		<RouterView></RouterView>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
